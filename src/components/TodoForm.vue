2730ebdf27110b7aa88da548fc6be964b131528f
doramagyari <doramagyari@yahoo.com>
2024-03-07 17:05:50 +0200

    Adding new to do to the list -TodoList, TodoForm

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <form @submit.prevent="addTodo">
+        <label for="newTodo">New Todo</label>
+        <input type="text" id="newTodo" v-model="newTodo"/>
+        <button type="submit">Add Todo</button>
+    </form>
+</template>
+
+<script setup lang="ts">
+
+import {ref, defineEmits } from 'vue';
+
+const newTodo = ref<string>("");
+const emit = defineEmits();
+
+function addTodo() {
+    emit('add-todo', newTodo.value);
+    newTodo.value = "";
+}
+
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <TodoForm @add-todo="handleAddTodo"/>
+        <ul>
+            <li v-for="(todo, index) in todos" :key="index">{{ todo }}</li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import {ref} from 'vue';
+import TodoForm from './TodoForm.vue';
+
+const todos = ref<string[]>([]);
+function handleAddTodo(newTodo : string) : void {
+    todos.value.push(newTodo)
+}
+
+</script>
