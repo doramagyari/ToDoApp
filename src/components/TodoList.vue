da4e201db8f7e285ede93195c997f058df9876bd
doramagyari <doramagyari@yahoo.com>
2024-03-11 11:41:42 +0200

    Fixed PR issues

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <TodoForm @add-todo="handleAddTodo"/>
-        <TodoRemoveAll @remove-all-todo="handleRemoveAllTodo"/>
+        <BaseButton @click="handleRemoveAllTodo">Clear All</BaseButton>
         <ul>
             <li v-for="(todo, index) in todos" :key="index">
                 {{ todo }}
-                <TodoRemove :index="index" @remove-todo="handleRemoveTodo" />
+                <BaseButton @click="handleRemoveTodo(index)" >Remove</BaseButton>
             </li>
         </ul>
     </div>
@@ -14,9 +14,8 @@
 <script setup lang="ts">
 
 import {ref} from 'vue';
-import TodoForm from './todo_crud/TodoForm.vue';
-import TodoRemove from './todo_crud/TodoRemove.vue';
-import TodoRemoveAll from './todo_crud/TodoRemoveAll.vue';
+import BaseButton from './ui/BaseButton.vue';
+import TodoForm from './pages/TodoForm.vue';
 
 const todos = ref<string[]>([]);
 
--- src/components/pages/TodoForm.vue
+++ src/components/pages/TodoForm.vue
@@ -2,16 +2,17 @@
     <form @submit.prevent="addTodo">
         <label for="newTodo">New Todo</label>
         <input type="text" id="newTodo" v-model="newTodo"/>
-        <button type="submit">Add Todo</button>
+        <BaseButton type="submit" >Add</BaseButton>
     </form>
 </template>
 
 <script setup lang="ts">
 
 import {ref } from 'vue';
+import BaseButton from '../ui/BaseButton.vue';
 
-const newTodo = ref<string>("");
 const emit = defineEmits(['add-todo']);
+const newTodo = ref<string>("");
 
 function addTodo() {
     emit('add-todo', newTodo.value);
